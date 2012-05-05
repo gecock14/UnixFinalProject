ac4e4e3197bf8487ed531b92018b66d0bffff2f8
Greg Ecock <gecock14@vt.edu>
2012-05-05 00:55:52 -0400

    Update Functionality

--- testFinal/testGame.py
+++ testFinal/testGame.py
@@ -9,6 +9,8 @@ global keys
 keys = 0
 global defeated
 defeated = []
+global visited
+visited = []
 global inventory
 inventory = []
 
@@ -21,35 +23,42 @@ class searchRoom:
     
 
     def enter(self):
-	print self.description
-	print "Rooms:"
+	print "\n\n\n", self.description
+	print "Rooms:",
 	for option in self.destinations:
 	    print option,
 	self.prompt()
 
 
     def prompt(self):
-	print "\nPick a room: "
 	self.checkForKey()
+	print "Pick a room: "
 	choice = raw_input('==> ')
 	if(choice in self.destinations and choice != 'Boss'):
 	    self.gotoRoom(self.destinations[choice])
-	elif(choice == 'Boss' and keys != 2):
-	    print "You need two keys to get in"
-	    self.gotoRoom(4)
-	elif(choice == 'Boss' and keys == 2):
-	    print "You face the Boss!"
-	    endGame()
+	    if(self.destinations[choice] == 5 and keys != 2):
+	    	print "You need two keys to get in"
+	    	self.gotoRoom(4)
+	    elif(self.destinations[choice] == 5 and keys == 2):
+	    	print "You face the Boss!"
+	    	endGame()
 	else:
-	    print "didnt work"
+	    print "Invalid Room"
+	    self.prompt()
+
 
     def gotoRoom(self, where):
 	rooms[where].enter()
 
+
     def checkForKey(self):
-	global keys
-	keys = keys + self.key
-	print "Keys: ", keys
+	if(self.name not in visited):
+	    global keys
+	    keys = keys + self.key
+	    visited.append(self.name)
+	print "\nKeys: ", keys
+	
+
 
 def endGame():
     exit(0)
@@ -61,8 +70,6 @@ except:
     stderr.write("Error opening rooms\n")
     exit(1)
 
-global keys
-keys = 0
 rooms = []
 ind = 0
 
